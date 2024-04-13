c8fb3ec87f57cda4452a98238be18bc596120ecf
ajeshs02 <ajeshs.dev@gmail.com>
2024-04-13 13:42:13 +0530

    updated SEO

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,14 +4,12 @@ export const metadata = {
   title: 'Ajesh S',
   description: 'Ajesh S portfolio in Next.Js-14',
   other: {
-    'theme-color': '#2c1250',
+    'theme-color': '#a374ff',
     'color-scheme': 'dark only',
-    'twitter:image':
-      'https://cdn.sanity.io/images/eivawakz/production/fd5d6ead28ca8974bf09ca61827ee5ba275990f7-1920x1080.webp',
+    'twitter:image': '/portfolio.webp',
     'twitter:card': 'summary_large_image',
-    'og:url': 'https://next-js-portfolio-phi-eight.vercel.app/',
-    'og:image':
-      'https://cdn.sanity.io/images/eivawakz/production/fd5d6ead28ca8974bf09ca61827ee5ba275990f7-1920x1080.webp',
+    'og:url': 'https://ajesh-s.vercel.app/',
+    'og:image': '/portfolio.webp',
     'og:type': 'website',
   },
 }
